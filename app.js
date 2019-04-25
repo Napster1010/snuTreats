83aa5d93dd984c78d72cf91b3f6eb1fd321b57de
Napster1010 <aditya.ndps@gmail.com>
2019-04-26 04:01:43 +0530

    Created some more routes and modified the previous code

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 
 var express = require("express");
 var app = express();
-var mysql = require('mysql')
+//var mysql = require('mysql')
 var bcrypt = require('bcrypt')
 var bodyParser = require("body-parser");
 
+//Connecting to postgreSQL database
+var pg = require('pg');
+var connectionString = "postgres://postgres:1010@localhost:5432/snuTreats";
+var client = new pg.Client(connectionString);
+client.connect();
 
+/*
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'password',
+  password : 'root',
   database : 'snuTreats'
-});
+});*/
+
+
+
 
 var saltRounds = 5;
 
@@ -19,36 +28,53 @@ app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({extended:true}));
 
 
-connection.connect()
+//connection.connect()
 
-//shops 
+
+
+//------------------------------------------------------USER ROUTES----------------------------------------------------------------
+
+//Return all the shops
 app.get("/shops",function(req,res){
-			connection.query('SELECT * FROM `shops` ', function (error, results, fields) {
-		 				res.json(JSON.stringify(results));
+			client.query('SELECT * FROM shops ', function (error, results, fields) {
+				if(error){
+					console.log(error);
+					res.status(500).send({message: error});
+				}else{
+					console.log(results);
+					res.status(200).send(results['rows']);
+				}
 			});
 
 		//	res.send("King Kushagr")
 });
 
 
-//inside indvidual shop
+//Return the menu of particular shop
 app.get("/shops/:id",function(req,res){
 	var id = req.params.id;
-	connection.query('SELECT * FROM `menuItems` WHERE shop_id =?',[id],function(error,results,fields) {
-		console.log(results);
-		res.json(JSON.stringify(results));
+	client.query('SELECT * FROM menu_items WHERE shop_id =$1',[id],function(error,results,fields) {
+		if(error){
+			console.log(error);
+			res.status(500).send({message: error});
+		}else{
+			res.status(200).send(results['rows']);
+		}
 	})
 });
 
 
 //individual item details
-app.get("/shops/:sid/item/:id",function(req,res){
-	var sid = req.params.sid;
+app.get("/shops/item/:id",function(req,res){
 	var id = req.params.id;
 
-	connection.query('SELECT * FROM `menuItems` WHERE shop_id =? AND id =?',[sid,id],function(error,results,fields) {
-		console.log(results);
-		res.json(JSON.stringify(results));
+	client.query('SELECT * FROM menu_items WHERE id =$1',[id],function(error,results,fields) {
+		if(error){
+			console.log(results);
+			res.status(500).send({message: error});
+		}else{
+			res.status(200).send(results['rows']);
+		}
 	});
 
 });
@@ -61,22 +87,23 @@ app.post("/login",function(req,res){
 		var passd = req.body.password;
 		//console.log(req.body);
 		var flag = 0;
-		      connection.query(`SELECT * FROM users WHERE username = ?`, [uname], function (error, results) {
-	    if (error) throw error;
+		      connection.query(`SELECT * FROM users WHERE username = $1`, [uname], function (error, results) {
+	    if (error){
+			res.status(500).send({message: error});
+		}
 
 	    if (results.length === 0) {
-	        return res.send({data: "details incorrect"})
+	        res.status(400).send({message: "details incorrect"})
 	    }
 
 	    bcrypt.compare(passd, results[0].password)
 	        .then(boolean => {
-
 	            if(!boolean){
-	                return res.send({data: "details incorrect"})
+	                res.send({message: "details incorrect"})
 	            }
 
-	            res.send({data: {username: results[0].username}})
-	        }).catch(error => {throw error});
+	            res.status(200).send({user: {username: results[0].username}})
+	        }).catch(error => {res.status(400).send({message: error})});
 	});
 			//res.send("No match found");
 });
@@ -90,24 +117,86 @@ app.post("/register",function(req,res){
 		var passd = req.body.password;
 		//console.log(req.body);
 		bcrypt.hash(passd,saltRounds,function(err,hash){
-			if(err)
+			if(err){
 				console.log(err);
-			var columns = ['username','password'];
-			var values = []
-			values.push(uname);
-			values.push(passd);
-			connection.query('INSERT INTO `users` (`username`,`password`) VALUES ("'+uname+'", "'+hash+'")',function (error, results, fields) {
-					if(error)
-						console.log(error)
-					res.send("registered");
+				res.status(400).send({message: `Couldn't hash the password`});
+			}
+			client.query('INSERT INTO users(username, password) VALUES ($1, $2)',[uname, hash],function (error, results, fields) {
+				if(error){
+					console.log(error);
+					res.status(500).send({message: error});
+				}else{
+					res.status(200).send({message: "User registered successfully"});
+				}
 			});
 		});
+});
+
+
+
+//------------------------------------------------------VENDOR ROUTES----------------------------------------------------------------
+
+//Add a new item to the shop
+app.post('/shops/:id', function(req, res){
+	var shopId = req.params.id;
+	var name = req.body.name;
+	var price = req.body.price;
+	client.query('insert into menu_items(shop_id, item_name, price, is_available) values($1, $2, $3, $4)',[shopId, name, price, true], function(error, result){
+		if(error){
+			console.log(error);
+			res.status(500).send({message: error});
+		}else{
+			res.status(200).send({message: "Menu Item added successfully"});
+		}
+	});	
+});
+
+//Update a menu item
+app.put('/shops/item/:id', function(req, res){
+	var id = req.params.id;
+	var name = req.body.name;
+	var price = req.body.price;
+	var isAvailable = req.body.is_available;
+
+	client.query('update menu_items set item_name=$1, price=$2, is_available=$3 where id=$4',[name, price, isAvailable, id], function(error, results){
+		if(error){
+			console.log(error);
+			res.status(500).send({message: error});
+		}else{
+			res.status(200).send({message: "Item details updated successfully"});
+		}
 	});
+});
+
+//------------------------------------------------------ADMIN ROUTES----------------------------------------------------------------
+
+//Add new vendor/shop
+app.post('/shops', function(req, res){
+	var vendorUsername = req.body.username;
+	var password = req.body.password;
+	var name = req.body.name;
+
+	bcrypt.hash(password, saltRounds, function(err, hash){
+		if(err){
+			console.log(err);
+			res.status(400).send({message: `Couldn't hash the password`});
+		}
+		client.query('insert into shops(name, vendor_uname, password, is_open) values($1, $2, $3, false)',[name, vendorUsername, hash], function(error, result){
+			if(error){
+				console.log(error);
+				res.status(500).send({message: error});
+			}else{
+				res.status(200).send({message: "User registered successfully"});
+			}
+		});
+	});
+});
 
 
 
 
 
+//Start the application on port 3000
 app.listen(3000,function(){
-	console.log("Server started");
+	console.log("Server started on port 3000");
 });
